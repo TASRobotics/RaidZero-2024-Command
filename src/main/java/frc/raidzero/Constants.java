232e6ee63ac30f38248d48f75da4df09694b688c
0x5b62656e5d <dev.benjamink@gmail.com>
2024-05-07 20:56:33 +0800

    Fix Git issues

--- src/main/java/frc/raidzero/Constants.java
+++ src/main/java/frc/raidzero/Constants.java
@@ -1,27 +1,5 @@
 package frc.raidzero;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
-
-
-
 public class Constants {
-
-    // TODO: Implement a reader of field feature locations in JSON format that then imports into the constant file
-    public static final class VisionConstants {
-        public static final String APRILTAG_CAM_NAME = "limelight";
-        public static final double XY_STDS = 0.1;
-        public static final double DEG_STDS = 1;
-
-        public static final String NOTE_CAM_NAME = "limelight-object";
-        public static final int NOTE_FILTER_SIZE = 5;
-
-        public static final Pose2d BLUE_SPEAKER_POSE = new Pose2d(Units.inchesToMeters(-1.5), Units.inchesToMeters(218.42), new Rotation2d());
-        public static final Pose2d RED_SPEAKER_POSE = new Pose2d(Units.inchesToMeters(652.73), Units.inchesToMeters(218.42), new Rotation2d());
-    }
-
-    public static final class LimelightConstants {
-        public static final String kLimelightName = "limelight";
-    }
+    
 }
--- src/main/java/frc/raidzero/dashboard/Tab.java
+++
@@ -1,8 +0,0 @@
-package frc.raidzero.dashboard;
-
-public class Tab {
-
-    public static final String MAIN = "Main";
-    public static final String SELECTION = "Selection";
-    public static final String DEBUG = "Debug";
-}
