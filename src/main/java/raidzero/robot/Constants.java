90575d4d5335bc1a0c52bc3b209a1baf8a599e1c
0x5b62656e5d <dev.benjamink@gmail.com>
2024-08-30 20:50:03 +0800

    Change note Limelight name constant

--- src/main/java/raidzero/robot/Constants.java
+++ src/main/java/raidzero/robot/Constants.java
@@ -152,7 +152,7 @@ public class Constants {
         public static final double XY_STDS = 0.1;
         public static final double DEG_STDS = 1;
 
-        public static final String NOTE_CAM_NAME = "limelight-object";
+        public static final String NOTE_CAM_NAME = "limelight-front";
         public static final int NOTE_FILTER_SIZE = 5;
 
         public static final Pose2d BLUE_SPEAKER_POSE = new Pose2d(Units.inchesToMeters(-1.5), Units.inchesToMeters(218.42), new Rotation2d());
