15fa11c21166364b13c59319789b2f8c7bac3536
0x5b62656e5d <dev.benjamink@gmail.com>
2024-09-19 12:14:51 +0800

    Update methods for GoToNote command

--- src/main/java/raidzero/robot/commands/GoToNote.java
+++ src/main/java/raidzero/robot/commands/GoToNote.java
@@ -1,21 +1,41 @@
 package raidzero.robot.commands;
 
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
+import raidzero.robot.subsystems.CommandSwerveDrivetrain;
+import raidzero.robot.subsystems.NeuralLimelight;
 
 public class GoToNote extends Command {
+
+    CommandSwerveDrivetrain swerve = CommandSwerveDrivetrain.system();
+
+    NeuralLimelight neuralLL = NeuralLimelight.getSystem();
+    
+    private final SwerveRequest.ApplyChassisSpeeds AutoRequest = new SwerveRequest.ApplyChassisSpeeds();
 
     public GoToNote() {}
 
     @Override
     public void execute() {
-
+        swerve.setControl(AutoRequest.withSpeeds(
+            ChassisSpeeds.fromRobotRelativeSpeeds(
+                0.0,
+                0.3,
+                0.0,
+                swerve.getPigeon2().getRotation2d()
+            )
+        ));
     }
 
     @Override
-    public boolean isFinished() {
-        return true;
+    public boolean isFinished() { // TY < 17 deg
+        return neuralLL.getNoteTy() > 17.0;
     }
 
     @Override
-    public void end(boolean interrupted) {} 
+    public void end(boolean interrupted) {
+        swerve.stop();
+    }
 }
