0d1ba722be66a7857d3ee28c1287f6707328098e
0x5b62656e5d <dev.benjamink@gmail.com>
2024-09-19 11:56:41 +0800

    Add GoToNote command

---
+++ src/main/java/raidzero/robot/commands/GoToNote.java
@@ -0,0 +1,21 @@
+package raidzero.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class GoToNote extends Command {
+
+    public GoToNote() {}
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    public void end(boolean interrupted) {} 
+}
